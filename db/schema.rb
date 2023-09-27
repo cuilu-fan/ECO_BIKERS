8d7fb544ab1da119a460eb869de40cf1f551af8b
shakar hassan <shakarmohammed2002@gmail.com>
2023-09-27 17:16:41 +0200

    models added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_133752) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_151037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bikes", force: :cascade do |t|
+    t.string "brand"
+    t.string "color"
+    t.string "size"
+    t.string "condition"
+    t.integer "age"
+    t.integer "refurbed_year"
+    t.float "price"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_bikes_on_category_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "bike_id", null: false
+    t.bigint "order_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_carts_on_bike_id"
+    t.index ["order_id"], name: "index_carts_on_order_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "purchase_date"
+    t.bigint "user_id", null: false
+    t.bigint "bike_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_orders_on_bike_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.string "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +80,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_133752) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bikes", "categories"
+  add_foreign_key "carts", "bikes"
+  add_foreign_key "carts", "orders"
+  add_foreign_key "carts", "users"
+  add_foreign_key "orders", "bikes"
+  add_foreign_key "orders", "users"
+  add_foreign_key "reviews", "users"
 end
