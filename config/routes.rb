c207248d5fee84f301868a174959cb90d5e9b92f
Juan Laborde <juan.laborde33@gmail.com>
2023-09-27 15:07:21 +0200

    Push the code with the correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  # Home Page
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   # Users routes
   get 'users', to: 'user#show'
