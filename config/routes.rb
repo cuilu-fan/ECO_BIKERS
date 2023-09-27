b069a2c9ea502df10ef2b5b774d47e57663da715
Juan Laborde <juan.laborde33@gmail.com>
2023-09-27 14:57:11 +0200

    Upload routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,29 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Users routes
+  get 'users', to: 'user#show'
+  get 'users/edit', to: 'user#edit'
+  patch 'users/update', to: 'user#update'
+
+  # Categories routes
+  get 'categories', to: 'categories#home'
+
+  # Bikes routes
+  get 'bikes/:id', to: 'bikes#show'
+
+  # Carts routes
+  get 'carts/:bike_id', to: 'carts#show'
+  delete 'carts/:bike_id', to: 'carts#index'
+  get 'carts/:id', to: 'carts#index'
+
+  # Orders routes
+  get 'orders/:id', to: 'orders#index'
+  get 'orders/:id/new', to: 'orders#new'
+  post 'orders/:id', to: 'orders#create'
+
+  # Reviews routes
+  get 'orders/:order_id/reviews', to: 'reviews#new'
+  post 'orders/:order_id/reviews', to: 'reviews#create'
 end
