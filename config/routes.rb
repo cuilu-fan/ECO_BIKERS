fbc9b9cc47f7826f657755ddefce9e0bf89cf9ac
shakar hassan <shakarmohammed2002@gmail.com>
2023-09-28 14:17:09 +0200

    category name in routes corrected to categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "category#index"
+  root "categories#index"
 
   resources :bikes do
     resources :orders, only: [:new, :create]
