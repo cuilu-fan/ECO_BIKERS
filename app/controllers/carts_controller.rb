36d4f3342dcf85090b050f290496dde87d647374
shakar hassan <shakarmohammed2002@gmail.com>
2023-10-04 18:00:04 +0200

    crats index and crete done T.B not styled

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,11 @@
 class CartsController < ApplicationController
-def index
-  @carts = Cart.all
-end
 
-def create
-  @cart = Cart.new(bike_id: params[:id], user: current_user)
-  raise
-end
+  def index
+    @carts = Cart.all
+  end
 
+  def create
+    @cart = Cart.new(bike_id: params[:id], user: current_user)
+    raise
+  end
 end
