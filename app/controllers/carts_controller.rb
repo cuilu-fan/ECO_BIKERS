7f33c8759a0c0f3b4ebc31f070073f393cd42141
shakar hassan <shakarmohammed2002@gmail.com>
2023-10-04 17:43:29 +0200

    crats index and crete done T.B not styled

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,11 @@
 class CartsController < ApplicationController
+def index
+  @carts = Cart.all
+end
+
+def create
+  @cart = Cart.new(bike_id: params[:id], user: current_user)
+  raise
+end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :bike
-  has_many :carts, dependent: :destroy
+  belongs_to :cart
 end
