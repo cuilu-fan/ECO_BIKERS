09a6e736883eb4697f398aaec5a5dc72460b05e4
Shakar-coder <134102951+Shakar-coder@users.noreply.github.com>
2023-10-06 15:00:19 +0200

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,11 @@
 class OrdersController < ApplicationController
-
   def index
     @orders = Order.all
   end
 
   def create
-
     @order = Order.new(cart_id: params[:cart_id], user: current_user, bike_id: params[:bike_id])
     @order.save
     redirect_to orders_path
-
   end
 end
